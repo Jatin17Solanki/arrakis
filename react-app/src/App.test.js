a19c5d4188b8012debba37d5ee1b2d98d5733f92
Jatin17Solanki <jatin.solanki1@learner.manipal.edu>
2022-08-10 11:12:27 +0530

    commented app.test.js

--- react-app/src/App.test.js
+++ react-app/src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders App component', () => {
-  render(<App />);
-  const { container } = render(<App />);
-  expect(container).toBeDefined();
-});
+//import { render, screen } from '@testing-library/react';
+//import App from './App';
+//
+//test('renders App component', () => {
+//  render(<App />);
+//  const { container } = render(<App />);
+//  expect(container).toBeDefined();
+//});
